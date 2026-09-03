af265eb0d93d6929615c6099c30ba5e48224535d
cloud2-k <susie.k.works@gmail.com>
2026-09-03 18:38:42 +0900

    refactor(common): 중복 아이콘 제거 및 단일 SVG로 통합
    - 중복 생성된 SVG 파일 일괄 삭제 및 파일명 변경
    - 도토리 스티커 컴포넌트 생성

--- src/pages/SamplePage.jsx
+++ src/pages/SamplePage.jsx
@@ -1,3 +1,5 @@
+import AcornSticker from "../components/common/AcornSticker";
+
 function SamplePage() {
   return (
     <ul>
@@ -12,6 +14,11 @@ function SamplePage() {
         {
           "3. src > App.jsx 파일의 router 내부 children 배열에 객체({ path, element })를 추가합니다."
         }
+      </li>
+      <li>
+        {/* 도토리 추가 예시 */}
+        <AcornSticker bgColor={"#d2e869"} />{" "}
+        <AcornSticker bgColor={"var(--color-sticker-blue-100)"} />
       </li>
     </ul>
   );
