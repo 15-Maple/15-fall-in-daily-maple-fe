a1ae73e5d435e51c1857e784b5eae17f1ce465b5
Kyungrae Kim <kkr6293@gmail.com>
2026-09-04 17:40:58 +0900

    feat(habit): 리뷰 피드백 반영

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import CreateLog from "./pages/createLog/CreateLog";
+import LogDetail from "./pages/LogDetail";
 import SamplePage from "./pages/SamplePage";
-import StudyDetail from "./pages/StudyDetail";
 
 import { ROUTES } from "./constants/routes";
 
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
       { path: ROUTES.HOME, element: <>홈</> },
       { path: ROUTES.SAMPLE, element: <SamplePage /> },
       { path: ROUTES.CREATELOG, element: <CreateLog /> },
-      { path: "/study-detail", element: <StudyDetail /> },
+      { path: ROUTES.LOG_DETAIL, element: <LogDetail /> },
     ],
   },
 ]);
