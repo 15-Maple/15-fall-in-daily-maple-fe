2a15b6aa454bd0631c20324e08a6e278406fd327
cloud2-k <susie.k.works@gmail.com>
2026-09-04 18:58:31 +0900

    feat(common): 공통 모달 컴포넌트 구현 및 사용 가이드 작성
    - React Portal을 활용한 전역 Modal 컴포넌트 구현
    - 4가지 모달 타입(alert, confirm, prompt, form) 분기 및 반응형 스타일 적용
    - 모달 내 사용을 위해 Button 컴포넌트 속성(variant="cancel", type="button") 확장
    - 모달 사용 가이드(docs/modal-guide.md) 추가

--- src/pages/SamplePage.jsx
+++ src/pages/SamplePage.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 import AcornSticker from "../components/common/AcornSticker";
+import Modal from "../components/common/Modal";
+import Button from "../components/ui/Button";
 import TimerButton from "../components/ui/TimerButton";
 
 function SamplePage() {
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  const [isPromptOpen, setIsPromptOpen] = useState(false);
+  const [isModalFormOpen, setIsModalFormOpen] = useState(false);
+
   return (
     <ul>
       페이지 및 라우트 추가 가이드
@@ -27,6 +36,72 @@ function SamplePage() {
         <TimerButton size="lg" type="stop" />
         <TimerButton size="sm" type="start" />
         <TimerButton size="lg" type="start" />
+      </li>
+      <li>
+        {/* 알럿창 */}
+        <Button size="xs" type="button" onClick={() => setIsAlertOpen(true)}>
+          알럿창
+        </Button>
+        <Modal
+          content="팝업 관련 메시지가 들어갑니다."
+          isOpen={isAlertOpen}
+          type="alert"
+          onClose={() => setIsAlertOpen(false)}
+        />
+      </li>
+      <li>
+        {/* 컨펌 */}
+        <Button size="xs" type="button" onClick={() => setIsConfirmOpen(true)}>
+          컨펌창
+        </Button>
+        <Modal
+          confirmText="예"
+          content="정말 나가시겠습니까?"
+          isOpen={isConfirmOpen}
+          type="confirm"
+          onClose={() => setIsConfirmOpen(false)}
+          onConfirm={() => {
+            console.log("필요 로직 실행");
+            setIsConfirmOpen(false);
+          }}
+        />
+      </li>
+      <li>
+        {/* 프롬프트 */}
+        <Button size="xs" type="button" onClick={() => setIsPromptOpen(true)}>
+          프롬프트버튼
+        </Button>
+        <Modal
+          confirmText="수정하러 가기"
+          content="권한이 필요해요!"
+          isOpen={isPromptOpen}
+          title="삭제 확인"
+          type="prompt"
+          onClose={() => setIsPromptOpen(false)}
+          onConfirm={() => {
+            console.log("필요 로직 실행");
+            setIsPromptOpen(false);
+          }}
+        >
+          input 박스가 들어가는 곳
+        </Modal>
+      </li>
+      <li>
+        {/* 폼모달 */}
+        <Button
+          size="xs"
+          type="button"
+          onClick={() => setIsModalFormOpen(true)}
+        >
+          폼모달
+        </Button>
+        <Modal
+          isOpen={isModalFormOpen}
+          type="form"
+          onClose={() => setIsModalFormOpen(false)}
+        >
+          전체 내용이 들어가는 곳
+        </Modal>
       </li>
     </ul>
   );
