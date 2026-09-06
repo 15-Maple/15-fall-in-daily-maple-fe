a67f82fde5fb73822b3c129f8230c922aba7ea0e
Kihong-s <golden7kihong@gmail.com>
2026-09-07 08:56:38 +0900

    feat(habit): 오늘의 습관(모달) 조회,생성, 수정, 삭제 API 연동

--- src/api/habit.js
+++ src/api/habit.js
@@ -4,3 +4,8 @@ export async function fetchTodayHabits(logId) {
   const data = await api.get(`/logs/${logId}/habits`);
   return data.items; // [{id, name, isChecked}, ...]
 }
+
+export async function deactivateHabit(habitId) {
+  const data = await api.patch(`/habits/${habitId}/deactivate`);
+  return data.habit;
+}
--- src/components/habitModal/TodayHabitsModal.jsx
+++ src/components/habitModal/TodayHabitsModal.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { deactivateHabit, fetchTodayHabits } from "../../api/habit.js";
 
 import Button from "../ui/Button.jsx";
 
@@ -7,19 +9,37 @@ import trashcanIcon from "../../assets/ic-trashcan.svg";
 import styles from "./TodayHabitsModal.module.css";
 
 function HabitsModal({ onClose }) {
-  const [habits, setHabits] = useState([
-    "미라클모닝 6시 기상",
-    "아침 챙겨 먹기",
-    "React 스터디 책 1챕터 읽기",
-    "스트레칭",
-    "영양제 챙겨 먹기",
-    "사이드 프로젝트",
-    "물 2L 먹기",
-  ]);
+  const [habits, setHabits] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleDelete = (indexToDelete) => {
-    setHabits(habits.filter((_, index) => index !== indexToDelete));
+  const logId = 3; //테스트용
+
+  useEffect(() => {
+    fetchTodayHabits(logId)
+      .then((data) => {
+        console.log("습관 API 데이터:", data);
+        setHabits(data);
+      })
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const handleDelete = async (habitId) => {
+    try {
+      await deactivateHabit(habitId);
+
+      setHabits(habits.filter((habit) => habit.id !== habitId));
+    } catch (error) {
+      console.error("습관 삭제 실패:", error);
+    }
   };
+
+  if (isLoading) return <p>불러오는 중...</p>;
+
+  if (error) {
+    return <p>에러가 발생했습니다: {error.message}</p>;
+  }
 
   return (
     <div className={styles.modalOverlay}>
@@ -27,14 +47,14 @@ function HabitsModal({ onClose }) {
         <h1 className={styles.title}>습관 목록</h1>
 
         <div className={styles.habitList}>
-          {habits.map((habit, index) => (
-            <div key={index} className={styles.habitItem}>
-              <div className={styles.habitName}>{habit}</div>
+          {habits.map((habit) => (
+            <div key={habit.id} className={styles.habitItem}>
+              <div className={styles.habitName}>{habit.name}</div>
 
               <button
-                aria-label={`${habit} 삭제`}
+                aria-label={`${habit.name} 삭제`}
                 className={styles.habitDelete}
-                onClick={() => handleDelete(index)}
+                onClick={() => handleDelete(habit.id)}
               >
                 <img
                   alt=""
