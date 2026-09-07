bb79b241fa234b845c3694bcc58f46b376b012a4
Kyungrae Kim <kkr6293@gmail.com>
2026-09-07 16:03:30 +0900

    feat(point): 포인트 조회ui 구현

--- src/api/reaction.js
+++ src/api/reaction.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import { api } from "./axios";
 
 //리액션조회
 export function getReaction(logId) {
-  return axios.get(`/api/logs/${logId}/reactions`);
+  return api.get(`/logs/${logId}/reactions`);
 }
 
 //리액션추가
 export function postReaction(logId, emoji) {
-  return axios.post(`/api/logs/${logId}/reactions`, { emoji });
+  return api.post(`/logs/${logId}/reactions`, { reactionType: emoji });
 }
--- src/pages/LogDetail.jsx
+++ src/pages/LogDetail.jsx
@@ -1,3 +1,4 @@
+import PointHistory from "../components/point/PointHistory";
 import Reaction from "../components/reaction/Reaction";
 
 import styles from "./LogDetail.module.css";
@@ -8,7 +9,7 @@ function LogDetail() {
       <section className={styles.content}>
         <Reaction />
 
-        <div>
+        {/* <div>
           <button>공유하기</button>
           <span>|</span>
           <button>수정하기</button>
@@ -19,7 +20,7 @@ function LogDetail() {
         <div>
           <button> 오늘의 습관</button>
           <button> 오늘의 집중</button>
-        </div>
+        </div> */}
 
         <h1 className={styles.title}>연우의 개발공장</h1>
         <div className={styles.sub}>
@@ -29,6 +30,7 @@ function LogDetail() {
           </p>
         </div>
         <div className={styles.pointText}>현재까지 획득한 포인트</div>
+        <PointHistory />
         <div className={styles.box}>
           <h2>습관기록표</h2>
           <p className={styles.noHabitText}>
