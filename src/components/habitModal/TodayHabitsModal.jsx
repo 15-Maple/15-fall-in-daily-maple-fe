e9237755d0fdf019554511c41925dc4ce8d3c016
Kihong-s <golden7kihong@gmail.com>
2026-09-07 17:14:37 +0900

    feat(habit): 새 습관 입력창 placeholder 동작 추가

--- src/components/habitModal/TodayHabitsModal.jsx
+++ src/components/habitModal/TodayHabitsModal.jsx
@@ -14,6 +14,9 @@ function HabitsModal({ id, onClose }) {
 
   const [isAdding, setIsAdding] = useState(false); // 새 습관 입력창 표시 여부
   const [newHabitName, setNewHabitName] = useState(""); // 새 습관 이름 저장
+  const [newHabits, setNewHabits] = useState([]); // 새로 추가한 습관 목록 (최대한 연관성 있는 이름으로 지어봤는데 구분이 될까요...)
+
+  const [placeholder, setPlaceholder] = useState("새로운 습관을 입력해주세요"); // 새로운 습관 input 안내 문구 저장
 
   const handleDelete = async (habitId) => {
     try {
@@ -23,6 +26,16 @@ function HabitsModal({ id, onClose }) {
     } catch (error) {
       console.error("습관 삭제 실패:", error);
     }
+  };
+
+  // Enter를 누르면 새 습관을 임시 목록에 추가
+  const handleAddHabit = () => {
+    if (!newHabitName.trim()) return;
+
+    setNewHabits([...newHabits, newHabitName]);
+
+    setNewHabitName("");
+    setIsAdding(false);
   };
 
   if (isLoading) return <p>불러오는 중...</p>;
@@ -53,16 +66,32 @@ function HabitsModal({ id, onClose }) {
               </button>
             </div>
           ))}
+
+          {/* 새로 입력한 습관 */}
+          {newHabits.map((habit, index) => (
+            <div key={index} className={styles.habitItem}>
+              <div className={styles.habitName}>{habit}</div>
+              <div className={styles.habitAddSpacer} />
+            </div>
+          ))}
         </div>
 
         {isAdding && ( // + 버튼을 누른 뒤에만 새 습관 입력창 표시
           <div className={styles.habitAddLayout}>
             <input
-              placeholder="새로운 습관을 입력해주세요"
+              placeholder={placeholder}
               type="text"
               value={newHabitName}
               className={styles.habitName}
+              onBlur={() => setPlaceholder("새로운 습관을 입력해주세요")} // input에서 나오고 아무 텍스트 없을 때 문구 다시 표시
               onChange={(e) => setNewHabitName(e.target.value)}
+              onFocus={() => setPlaceholder("")} // 안내 문구 숨기기
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  // 습관 제목 입력하고 엔터 기능
+                  handleAddHabit();
+                }
+              }}
             />
             <div className={styles.habitAddSpacer} />
           </div>
