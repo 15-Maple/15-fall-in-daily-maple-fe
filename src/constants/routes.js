cf79862ead6a982cbd0efc9948e7c225bd000472
cloud2-k <susie.k.works@gmail.com>
2026-09-08 18:29:39 +0900

    feat(focus): 오늘의 집중 페이지 기본 뼈대 구성 (WIP)
    - 영역 분리 및 기본 레이아웃 배치
    - 기능 및 상세 스타일링은 추후 작업 예정

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,9 +2,8 @@ export const ROUTES = {
   HOME: "/",
   // 라우트 추가는 맨 아래 SAMPLE을 참고하여, 이 줄 아래부터 부탁드립니다.
   CREATELOG: "/createlog",
-
-  SAMPLE: "/sample", // 새로운 페이지를 추가할 때 참고하는 샘플 경로입니다.
-
   LOG_DETAIL: "/logdetail",
   TODAY_HABITS: "/todayhabits",
+  TODAY_FOCUS: "/today-focus",
+  SAMPLE: "/sample", // 새로운 페이지를 추가할 때 참고하는 샘플 경로입니다.
 };
