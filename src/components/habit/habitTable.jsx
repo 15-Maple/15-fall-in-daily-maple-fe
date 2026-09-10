6c83b295f41336c793bf4f81083814e5a466f84b
Kyungrae Kim <kkr6293@gmail.com>
2026-09-10 09:37:21 +0900

    feat(habit): 습관기록표 목데이터 추가

--- src/components/habit/habitTable.jsx
+++ src/components/habit/habitTable.jsx
@@ -1,9 +1,54 @@
 import styles from "./habit.module.css";
 
 function HabitTable() {
+  const habits = [
+    {
+      name: "도토리 100개 줍기",
+      dotoris: [true, true, true, true, true, true, false],
+    },
+    {
+      name: "다람쥐 친구구하기",
+      dotoris: [true, true, true, true, true, true, false],
+    },
+    {
+      name: "스쿼시 하기",
+      dotoris: [true, true, true, true, true, true, false],
+    },
+    {
+      name: "낮잠자기",
+      dotoris: [true, true, true, true, true, true, false],
+    },
+    {
+      name: "오버워치 하기",
+      dotoris: [true, true, true, true, true, true, false],
+    },
+    {
+      name: "고양이 놀아주기",
+      dotoris: [true, true, true, true, true, true, false],
+    },
+  ];
+
+  const days = ["월", "화", "수", "목", "금", "토", "일"];
+
   return (
     <div div className={styles.box}>
       <h2 className={styles.title}>습관 기록표</h2>
+
+      <div>
+        {days.map((day) => (
+          <span key={day}>{day}</span>
+        ))}
+      </div>
+
+      {habits.map((habit) => (
+        <div key={habit.name}>
+          <span>{habit.name}</span>
+
+          {habit.dotoris.map((dotori, index) => (
+            <span key={index}>{dotori ? "o" : "x"}</span>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
