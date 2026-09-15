cc414d6929994e823a178373bd3a14b31d0d9c78
cloud2-k <susie.k.works@gmail.com>
2026-09-15 16:17:49 +0900

    chore(env): 절대경로(@/) alias 환경 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from "url";
+
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -5,4 +7,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
